aa0cea1b011240eeee3c970032ca45bcd784f621
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-07-04 03:03:11 +0530

    Listing addition php code seperated in seperate php file "saveListing.php"and ExpiryDate column and input added

--- profile.php
+++ profile.php
@@ -9,36 +9,6 @@ if(!isset($_SESSION["username"])){
 	exit();
 }
 
-$username = $_SESSION['username'];
-$tablename = "foods";
-
-if($_SERVER['REQUEST_METHOD']=="POST"){
-
-	$sql = "USE FoodShare;";
-	$conn->query($sql);
-
-	if($_POST['purpose']=="addListing"){
-
-		$type = $_POST['type'];
-		$title = $_POST['title'];
-		$desc = $_POST['desc'];
-		$latitude = $_POST['latitude'];
-		$longitude = $_POST['longitude'];
-		$pickupTime = $_POST['time'];
-		$creationDate = date('Y-m-d H:i:s');
-
-		$stmt = $conn->prepare("INSERT INTO $tablename(Username,Type,Title,Description,Latitude,Longitude,PickupTime,CreationTime) VALUES(?,?,?,?,?,?,?,?);");
-		if(!$stmt){
-				echo "Error preparing statement ".htmlspecialchars($conn->error);
-			}
-		$stmt->bind_param("ssssddss",$username,$type,$title,$desc,$latitude,$longitude,$pickupTime,$creationDate);
-		$stmt->execute();
-		$stmt->close();
-
-	}
-
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -56,9 +26,9 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
   	<link rel="stylesheet" type="text/css" href="css/profile.css">
   	<link rel="stylesheet" type="text/css" href="css/topnav.css">
   	<link rel="stylesheet" type="text/css" href="css/sidenav.css">
-  	<link rel="stylesheet" type="text/css" href="css/modal.css">
+  	<link rel="stylesheet" type="text/css" href="css/addListingModal.css">
   	<link rel="stylesheet" type="text/css" href="css/map.css">
-  	
+
 </head>
 
 <body>
@@ -66,11 +36,12 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 		<a id="titleId" class="title" onclick="home()">FoodShare</a>
 		<a class="options" href="#home" onclick="home()">Home</a>
 	  	<a class="active options" onclick="profile()">Profile</a>
+	  	<a class="options" onclick="messages()">Messages</a>
 	</div>	
 	<div class="sidenav" id="sidenav">
 		<a class="sidenavlinks active" onclick="">View Profile</a>
 		<a class="sidenavlinks" onclick="openNewListingModal();">Add Listing</a>
-		<a class="sidenavlinks" onclick="">My Listings</a>
+		<a class="sidenavlinks" onclick="myListings()">My Listings</a>
 		<a class="sidenavlinks" onclick="logout();">Logout</a>
 	</div>
 	<div class="modal" id="modalId"> 
@@ -88,15 +59,17 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 				</div>			
 				<div><input id="titleInputId" class="inputClass" type="text" name="title" placeholder="Title"/></div>	
 				<div><input id="descInputId" class="inputClass" type="text" name="description" placeholder="Description"/></div>	
-				<input id="pac-input" class="controls" type="text" name="location" placeholder="Approximate Pick-up Location"/>
+				<div><input id="locationInputId" class="inputClass" type="text" name="location" placeholder="Approximate Pick-up Address"/></div>
+				<input id="pac-input" class="controls" type="text" name="location" placeholder="Locate by place"/>
 				<div id="map"></div>
 				<div><button id="saveLocationId" onclick="saveLocation();">Save Location</button></div>
 				<div><input id="timeInputId" class="inputClass" type="text" name="time" placeholder="Pick-up Times Eg. Monday evening"/></div>	
+				<div>Expiry Date: <input id="expiryDateId" type="date" name="expiryDate"></div>
 				<div><input id="submitInputId" class="inputClass" type="submit" name="submitList" value="Add" onclick="addListing();"></div>
 			</div>
 		</div>
 	</div>
-	<div id="activityRegion" class="main" style="margin-top: 20vh;"></div>
+	<div id="listingRegion" class="main" style="margin-top: 20vh;"></div>
 
 <script type="text/javascript">
 
@@ -128,6 +101,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 		document.getElementById("descInputId").placeholder = "Description";
 		document.getElementById("timeInputId").value = "";
 		document.getElementById("timeInputId").placeholder = "Pick-up Times Eg. Monday evening";
+		document.getElementById("expiryDateId").value = new Date().toDateInputValue();
 	}
 
 	window.onclick = function(event) {
@@ -137,11 +111,12 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 	}	
 
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=YOUR-API-KEY&libraries=places&callback=initAutocomplete" async defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLayRaJZWWbmSToJZd9UHgwp5QNVme5gw&libraries=places&callback=initAutocomplete" async defer></script>
 <script src="js/config.js"></script>
 <script src="js/map.js"></script>
 <script src="js/functions.js"></script>
 <script src="js/profile.js"></script>
-
+<script src="js/generic.js"></script>
+<script src="js/createCard.js"></script>
 </body>
 </html>
