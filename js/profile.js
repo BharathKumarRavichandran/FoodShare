286a091144673310ad42f7d56c858e2599ac2c64
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-07-04 03:03:54 +0530

    ExpiryDate input added and function to get listings added

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
 var i=0;
 var j=0;
-var activityRegion = document.getElementById("activityRegion");
+var cards = 0;
+var listingRegion = document.getElementById("listingRegion");
 var modal = document.getElementById("modalId");
 var selectId = document.getElementById("modalSelectId");
 
@@ -14,6 +15,7 @@ else{
 
 function openNewListingModal(){
 
+	document.getElementById("expiryDateId").value = new Date().toDateInputValue();
 	modal.style.display = "block";
 
 }
@@ -23,9 +25,8 @@ function addListing(){
 	var type = selectId.options[selectId.selectedIndex].text;
 	var title = document.getElementById("titleInputId").value;
 	var desc = document.getElementById("descInputId").value;
-	var latitude = markerLat;
-	var longitude = markerLng;
 	var time = document.getElementById("timeInputId").value;
+	var expiryDate = document.getElementById("expiryDateId").value;
 	var purpose = "addListing";
 
 	var xmlhttp;
@@ -35,8 +36,13 @@ function addListing(){
 	 else{
 	  	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	var params =  "type="+type+"&title="+title+"&desc="+desc+"&latitude="+latitude+"&longitude="+longitude+"&time="+time+"&purpose="+purpose;
-	var url = "profile.php";
+
+	var params =  "type="+type+"&title="+title+"&desc="+desc+"&address="+saveAddress+"&latitude="+saveLatitude+"&longitude="+saveLongitude+"&time="+time+"&expiryDate="+expiryDate+"&purpose="+purpose;
+	var url = "saveListing.php";
+	xmlhttp.onreadystatechange = function(){
+		if(this.readyState==4&&this.status==200){
+		}
+	};
 	xmlhttp.open("POST",url,true);
 	xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 	xmlhttp.send(params);
@@ -47,6 +53,49 @@ function addListing(){
 	document.getElementById("descInputId").placeholder = "Description";
 	document.getElementById("timeInputId").value = "";
 	document.getElementById("timeInputId").placeholder = "Pick-up Times Eg. Monday evening";
+	document.getElementById("expiryDateId").value = new Date().toDateInputValue();
 	modal.style.display = "none";
 
+}
+
+function myListings(){
+
+	while(listingRegion.firstChild){
+		listingRegion.removeChild(listingRegion.firstChild);
+	}
+
+	var xmlhttp;
+	if (window.XMLHttpRequest){
+	  		xmlhttp = new XMLHttpRequest();
+	} 
+	 else{
+	  	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	var url = "getListings.php";
+	var data;
+	var params = "purpose=self";
+	xmlhttp.onreadystatechange = function(){
+		if(this.readyState==4&&this.status==200){
+			cards = 0;
+			data = JSON.parse(this.responseText);
+			for(var u=0;u<data.length;u++){
+				createCard(cards,data[u].Type,data[u].Title,data[u].Description,data[u].Address,data[u].PickupTime,data[u].ExpiryDate,data[u].CreationTime);
+			}
+			if(!listingRegion.firstChild){
+				noListingDisplay();
+			}
+		}
+	};
+	xmlhttp.open("POST",url,true);
+	xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	xmlhttp.send(params);
+
+}
+
+function noListingDisplay(){
+	var div = document.createElement("div");
+	var divText = document.createTextNode("No Listings added by you!");
+	div.appendChild(divText);
+	listingRegion.appendChild(div);
+	div.setAttribute("class","no-listings card bg-light");
 }
