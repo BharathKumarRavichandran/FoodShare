72578dd9f2616f23e3250016ac9f4d27ded9e724
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-07-04 12:27:09 +0530

    Added Following, Followers column in table users

--- createDb.php
+++ createDb.php
@@ -10,7 +10,9 @@ $sql = "CREATE TABLE IF NOT EXISTS user(
 		id INT(100) NOT NULL AUTO_INCREMENT,
 		username VARCHAR(100) NOT NULL,
 		email VARCHAR(320) NOT NULL,
-		password VARCHAR(128) NOT NULL,
+		password VARCHAR(256) NOT NULL,
+		Following VARCHAR(500) NOT NULL,
+		Followers VARCHAR(500) NOT NULL,
 		PRIMARY KEY (id,username)
 		)";
 $conn->query($sql);
