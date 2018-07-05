6b0ee7b7fbf23f108b2375f09aee2958a213c7ba
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-07-05 16:30:43 +0530

    Added code to retrieve listings of other users

--- getListings.php
+++ getListings.php
@@ -45,6 +45,32 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 
 	}
 
+	else if($_POST["purpose"]=="others"){
+
+		$viewUser = $_SESSION["viewUser"];
+
+		$stmt = $conn->prepare("SELECT * FROM $tablename WHERE Username = ?");
+		if(!$stmt){
+			echo "Error preparing statement ".htmlspecialchars($conn->error);
+		}
+		$stmt->bind_param("s",$viewUser);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$stmt->close();
+
+		$listingsData = array();
+
+		if($result->num_rows>0){
+			while($row = $result->fetch_assoc()){
+
+				$r = array('Username'=>$row["Username"],'Type'=>$row["Type"],'Title'=>$row["Title"],'Description'=>$row["Description"],'Address'=>$row["Address"],'Latitude'=>$row["Latitude"],'Longitude'=>$row["Longitude"],'PickupTime'=>$row["PickupTime"],'ExpiryDate'=>$row["ExpiryDate"],'CreationTime'=>$row["CreationTime"]);
+				array_push($listingsData,$r);	
+			}
+		}	
+		echo json_encode($listingsData);
+
+	}
+
 	else if($_POST["purpose"]=="all"){
 
 		$stmt = $conn->prepare("SELECT * FROM $tablename");
