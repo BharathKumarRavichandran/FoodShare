69d1d76fdd589068a03e7f6888022b498bbe3d2c
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-07-05 16:10:29 +0530

    Added function viewProfileChipClick()

--- js/viewProfileClick.js
+++ js/viewProfileClick.js
@@ -25,3 +25,31 @@ function viewProfileBtnClick(y){
 	xmlhttp.send();
 
 }
+
+function viewProfileChipClick(y){
+
+	var idAttr = y.getAttribute("id");
+    var res = idAttr.split("chipDiv");
+    var k = parseInt(res[1]);
+    var viewUser = document.getElementById("username"+k).innerHTML;
+
+	var xmlhttp;
+	if (window.XMLHttpRequest){
+	  		xmlhttp = new XMLHttpRequest();
+	} 
+	 else{
+	  	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	var data;
+	var url = "setViewUser.php?viewUser="+viewUser;
+	xmlhttp.onreadystatechange = function(){
+	    if(this.readyState==4&&this.status==200){
+	    	console.log(this.responseText); 
+	    	window.location = "viewProfile.php";
+	    }
+	};
+	xmlhttp.open("GET",url,true);
+	xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	xmlhttp.send();
+
+}
